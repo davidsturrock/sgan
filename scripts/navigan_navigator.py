6fd2d28da6e348a41c7210e49829b52ac618e40d
aru-robots <aru.robots@gmail.com>
2023-01-26 10:56:26 -0500

    Clean Up

--- scripts/navigan_navigator.py
+++ scripts/navigan_navigator.py
@@ -1,4 +1,3 @@
-import math
 import pathlib
 import time
 import sys
@@ -10,53 +9,25 @@ from sgan.utils import relative_to_abs, abs_to_relative, Plotter
 sys.path.insert(0, '/home/administrator/code/aru-core/build/lib')
 # sys.path.insert(0, '/home/david/code/aru-core/build/lib')
 sys.path.insert(0, '/usr/local/lib/python3.6/dist-packages/cv2/python-3.6')
+
 import numpy as np
 import rospy
-import std_msgs
 from geometry_msgs.msg import Twist, Point, Pose
-# import aru_py_logger
-from utilities.Transform import distance_and_yaw_from_transform
-# from aru_sil_py.utilities.Transform import distance_and_yaw_from_transform
+
+
 from nav_msgs.msg import Odometry
 from scipy.spatial.transform import Rotation as R
 
 _DEVICE_ = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-class ControlParameters:
-    __slots__ = "dthresh", "ythresh", "pz", "max_x", "max_z"
-
-    def __init__(self, args):
-        """
-        self.p_z: float = args.angular_gain
-        self.dist_thresh: float = 1
-        self.yaw_thresh: float = 30
-        self.max_linear_vel: float = args.max_x
-        self.max_angular_vel: float = args.max_z
-        """
-        self.dthresh: float = args.distance_threshold
-        self.ythresh: float = args.yaw_threshold
-        self.pz: float = 1
-        self.max_x: float = args.max_x
-        self.max_z: float = args.max_z
-
-
-# def create_tf_logger(logfolder=pathlib.Path('logs'), logname=None,
-#                      overwrite: bool = True) -> aru_py_logger.TransformLogger:
-#     if logname is None:
-#         logname = f'live_tfs_log_{time.strftime("%d-%m-%y-%H:%M:%S")}.monolithic'
-#     pathlib.Path(logfolder).mkdir(parents=True, exist_ok=True)
-#     logfile = logfolder / logname
-#     return aru_py_logger.TransformLogger(str(logfile), overwrite)
-
-
 class Navigator:
 
-    def __init__(self, args, model_path, agents=9, rate=1, odom_topic='odometry/filtered'):
+    def __init__(self, model_path, agents=9, rate=1, odom_topic='odometry/filtered'):
         self.last_times = np.ones(agents) * time.perf_counter()
+
         self.odom_callback_status = False
-        self.tracked_agents = []
-        self.husky_odom = []
+
         self.odom_topic = odom_topic
         self.plotter = Plotter()
         self.goal = Point(0, 0, 0)
@@ -75,16 +46,11 @@ class Navigator:
         self.agents = agents
         # self.obs_traj = torch.zeros((self.obs_len, 1 + agents, 2), device=_DEVICE_)
         # TODO consider best solution to init agent values
-        # Initialise agents far awa to avoid influencing Husky planned path
+        # Initialise agents far away to avoid influencing Husky planned path
         self.obs_traj = torch.ones((self.obs_len, 1 + agents, 2), device=_DEVICE_) * 100
         self.obs_traj[::, 0] = 0
-        self.published_points = []
-        self.tfs = []
-        self.tf_to_last_loc = np.eye(4)
-        # self.logger = create_tf_logger() if args.log else None
-        self.verbose: bool = args.verbose
-        self.control_params: ControlParameters = ControlParameters(args)
-        rospy.init_node('naviganNavigator', anonymous=True)
+
+        rospy.init_node('SocialNav', anonymous=True)
 
         self.last_callback = time.perf_counter()
         self.odom_last_callback = time.perf_counter()
@@ -93,84 +59,23 @@ class Navigator:
         self.odom_sub = rospy.Subscriber(self.odom_topic, Odometry, self.odom_callback)
         self.ped_sub = rospy.Subscriber('/tracked/pedestrians', Point, self.callback)
         self.goal_sub = rospy.Subscriber('/goal', Point, self.goal_callback)
-        # self.publisher: rospy.Publisher = rospy.Publisher(args.pub_topic, Twist, queue_size=1, latch=True)
         self.publisher: rospy.Publisher = rospy.Publisher('/pose', Pose, queue_size=1, latch=True)
 
     def sleep(self):
         self.rate.sleep()
 
-    def goal_step(self, tf):
-        with np.printoptions(precision=2, suppress=True):
-            # print(f'Initial TF:\n{tf}')
-            # x = tf[2, 3]
-            # # curr_x = self.odom.pose.position.x
-            # # curr_y = self.odom.pose.position.y
-            # rot_mat = R.from_matrix(tf[0:3, 0:3])
-            # # turn_angle = math.atan2(curr_y, curr_x) * 180 / math.pi
-            # tf_turn_angle = rot_mat.as_euler('zyx', degrees=True)[0]
-            # # print(f"TF yaw {tf_turn_angle:.2f}")
-            #
-            # quat = self.odom.pose.orientation
-            # pose = R.from_quat([quat.x, quat.y, quat.z, quat.w])
-            # curr_yaw = pose.as_euler('zyx', degrees=True)[0]
-            # # print(f'Final turn angle: = {tf_turn_angle:.2f} - {curr_yaw:.2f} = {tf_turn_angle - curr_yaw:.2f}')
-            # # # print(f'Turn angle for ({y - curr_y:.2f}, {x - curr_x:.2f}): {turn_angle:.2f} deg.')
-            # turn_angle = curr_yaw - tf_turn_angle
-            # r = R.from_euler('z', turn_angle, degrees=True)
-            # rot = np.array(r.as_matrix())
-            # tf = np.eye(4)
-            # tf[0:3, 0:3] = rot
-            # tf[2, 3] = x
-            # print(f'New TF:\n{tf}')
-            pose = Pose()
-            # pose_mat = R.from_matrix(tf[0:3, 0:3])
-            # pose_quat = pose_mat.as_quat()
-            # pose.orientation.x = pose_quat[0]
-            # pose.orientation.y = pose_quat[1]
-            # pose.orientation.z = pose_quat[2]
-            pose.orientation.w = 1
-            pose.position.x = -tf[0,0]
-            pose.position.y = tf[0,1]
+    def goal_step(self, predictions, step_horizon=11):
+        """predictions: absolute coordinate predictions from sgan network of shape(12, no.agents, 2)
+            Row 0 is predictions for husky.
+            step_horizon: index of predictions to send as goal to move_base_mapless_demo controller"""
 
-            self.publisher.publish(pose)
-            # if x < self.control_params.dthresh and abs(turn_angle) < self.control_params.ythresh:
-            #     msg = self.controller(tf)
-            #     self.publisher.publish(msg)
-            # if x > self.control_params.dthresh:
-            #     print(f'{x:.2f} > {self.control_params.dthresh}')
-            # if abs(turn_angle) > self.control_params.ythresh:
-            #       print(f'{abs(turn_angle):.2f} > {self.control_params.ythresh}')
+        pose = Pose()
+        pose.orientation.w = 1
+        # Inver x value to deal with network bias issue. To be fixed
+        pose.position.x = -predictions[step_horizon, 0, 0]
+        pose.position.y = predictions[step_horizon, 0, 1]
 
-    def controller(self, tf):
-        """Controller now assumes TF is within bounds. Check must be performed at higher level"""
-        cmd_vel = Twist()
-        if np.array_equal(tf, np.eye(4)):
-            return cmd_vel
-        # TODO Check below comment
-        #  Invert for ORB to give correct left and right action (could also just flip sign of yaw)
-        # tf = np.linalg.inv(tf)
-        distance, yaw = distance_and_yaw_from_transform(tf)
-        pose = R.from_matrix(tf[0:3, 0:3])
-        yaw = pose.as_euler('zyx', degrees=True)[0]
-        yaw = -yaw
-        # print(f'Controller yaw: {yaw:.2f}')
-        # print(f'Yaw: {yaw} deg.')
-        # if logging, log the tf for path tracking purposes
-        # if self.logger is not None:
-        # TODO add source and dest timestamps to logging
-        # self.logger.write_to_file(tf.astype(dtype=np.float64), 0, 1)
-        cmd_vel.angular.z = -self.control_params.pz * (0 - yaw)
-        # Bound the output of controller to within max values
-        if cmd_vel.angular.z > self.control_params.max_z:
-            cmd_vel.angular.z = self.control_params.max_z
-        elif cmd_vel.angular.z < - self.control_params.max_z:
-            cmd_vel.angular.z = -self.control_params.max_z
-
-        # Use constant fwd speed if transform is within thresholds
-        cmd_vel.linear.x = self.control_params.max_x
-        print(f'Yaw error {0 - yaw:.2f} deg')
-        print(f'Max/ Curr Angular Vel {self.control_params.max_z:.2} / {cmd_vel.angular.z:.2}')
-        return cmd_vel
+        self.publisher.publish(pose)
 
     def callback(self, tracked_pts: Point):
         """update_obs_traj when a new list msg of tracked pts are received from the object tracker
@@ -183,7 +88,7 @@ class Navigator:
         # print(f'Y = -x + obs_traj[-1, 0 ,1] = {-tracked_pts.x:.2f} + {self.obs_traj[-1, 0, 1].item():.2f} = {-tracked_pts.x + self.obs_traj[-1, 0, 1].item():.2f}m')
         # # print(f'Callback rate {1 / (time.perf_counter() - self.last_callback):.2f}Hz')
         # print(
-            # f'Agent {int(tracked_pts.z)} tracked {time.perf_counter() - self.last_times[int(tracked_pts.z)]:.2f}s ago.')
+        # f'Agent {int(tracked_pts.z)} tracked {time.perf_counter() - self.last_times[int(tracked_pts.z)]:.2f}s ago.')
         if time.perf_counter() - self.last_times[int(tracked_pts.z)] < 0.4:
             return
 
@@ -241,86 +146,37 @@ class Navigator:
         self.goal.y -= self.obs_traj[-1, 0, 1].item()
         self.goal_status = True
 
-    def seek_live_goal(self, agent_id=0, x=40, y=10, title='live_exp', filename=None):
-        # self.goal = Point(x - self.obs_traj[-1, 0, 0].item() , y - self.obs_traj[-1, 0, 1].item(), 0)
+    def seek_live_goal(self, agent_id=0, x=40, y=10):
         with torch.no_grad():
-            pred_traj_gt = torch.zeros(self.obs_traj.shape, device=_DEVICE_)
-            # obs_traj_rel = abs_to_relative(self.obs_traj)
-            # obs_traj_rel[::, 1] = 0
-            # self.obs_traj[::, 1] = 0
-            # obs_traj_rel = torch.ones((self.obs_len, 1, 2), device=_DEVICE_) * 0.8
+
             seq_start_end = torch.tensor([0, self.obs_traj.shape[1]], device=_DEVICE_).unsqueeze(0)
-            # seq_start_end = torch.tensor([0, 1], device=_DEVICE_).unsqueeze(0)
-            with np.printoptions(precision=3, suppress=True):
-                goal_state = torch.zeros((1, 1, 2), device=_DEVICE_)
 
-                # goal_state[0, agent_id] = pred_traj_gt[-1, agent_id]
-                # goal_state[0, agent_id, 0] = x
+            goal_state = torch.zeros((1, 1, 2), device=_DEVICE_)
+            goal_state[0, agent_id, 0] = self.goal.x - self.obs_traj[-1, 0, 0].item()
+            goal_state[0, agent_id, 1] = self.goal.y - self.obs_traj[-1, 0, 1].item()
 
-                goal_state[0, agent_id, 0] = self.goal.x - self.obs_traj[-1, 0, 0].item()
-                goal_state[0, agent_id, 1] = self.goal.y - self.obs_traj[-1, 0, 1].item()
-                # print(f'X {self.goal.x:.2f}, Y {self.goal.y:.2f}')
-                # print(goal_state.shape)
-            # ota = self.obs_traj.numpy()
-            ptga = pred_traj_gt.numpy()
-            # self.obs_traj[::, 0] = self.husky_odom[-8::]
-            # self.obs_traj[::, 1::] = self.tracked_agents[-8::]
-            # with np.printoptions(precision=2, suppress=True):
-            #     print(self.obs_traj[::, 0:2, 0].T)
-            #     print(self.obs_traj[::, 0:2, 1].T)
-            # print(self.obs_traj[, 1].T)
             # TODO Resolve pred imbalance for now flip x - axis
-            # obs_traj = self.obs_traj
-            # obs_traj[::, ::, 0] = -obs_traj[::, ::, 0]
             obs_traj_rel = abs_to_relative(self.obs_traj)
             pred_traj_fake_rel = self.generator(self.obs_traj, obs_traj_rel, seq_start_end, goal_state, goal_aggro=0.5)
-            # self.obs_traj[:-1, 0] = self.obs_traj.clone()[1:, 0]
 
-            # with np.printoptions(precision=2, suppress=True):
-            #     print(f'Rel Pred Ped 0:\n{pred_traj_fake_rel[::, 1].T}')
-            #     print(f'Abs Observed Ped 0:\n{self.obs_traj[::, 1].T}')
-            #     print(f'Rel Observed Ped 0:\n{obs_traj_rel[::, 1].T}')
             start_pos = self.obs_traj[-1]
             ptfa = relative_to_abs(pred_traj_fake_rel, start_pos=start_pos)
+            # Make separate variable to plot. Flipped along x-axis to look visually correct.
             pred_traj_to_plot = ptfa.clone()
             pred_traj_to_plot[::, ::, 0] = -pred_traj_to_plot[::, ::, 0]
 
-            # self.obs_traj[-1, 0] = ptfa[0, 0]
             obs_traj_to_plot = self.obs_traj.clone()
             obs_traj_to_plot[::, ::, 0] = -obs_traj_to_plot[::, ::, 0]
-            # with open(filename, 'a') as f:
-            #     p = ptfa.numpy()
-            #     f.write(f'{p[0, 0, 0]:.3f}\t{p[0, 0, 1]:.3f}\n')
 
-            # print(ptfa[::,0].T)
-            # ptfa = relative_to_abs(pred_traj_fake_rel)
-
-            # plot_trajectories(ota, ptga, ptfa, seq_start_end)
             self.plotter.xlim = [obs_traj_to_plot[-1, 0, 0] + -5, obs_traj_to_plot[-1, 0, 0] + 5]
             self.plotter.ylim = [obs_traj_to_plot[-1, 0, 1] + -5, obs_traj_to_plot[-1, 0, 1] + 5]
-            # abs_goal = [goal_state[0, 0, 0] + self.obs_traj[-1, 0, 0], goal_state[0, 0, 1] + self.obs_traj[-1, 0, 1]]
+
             self.plotter.display(
                 title=f'\nRel Goal {goal_state[0, 0, 0].item():.2f}m {goal_state[0, 0, 1].item():.2f}m\n' \
                       f'Abs Goal {self.abs_goal[0]:.2f}m {self.abs_goal[1]:.2f}m', ota=obs_traj_to_plot,
                 ptfa=pred_traj_to_plot,
                 goal_centre=self.abs_goal, sse=seq_start_end)
 
-        # for i, ped in enumerate(self.obs_traj.permute(1, 0, 2)):
-        #     if i == agent_id:
-        #         print(f'Ped {i} observed traj\tX\n\t\t\t\t\tY\n{ped.T}')
-        # for i, ped in enumerate(ptfa.permute(1, 0, 2)):
-        #     if i == agent_id:
-        #         print(f'Ped {i} predicted\tX\n\t\t\t\t\tY\n{ped.T}')
         return ptfa, pred_traj_fake_rel
 
 
-def get_slice(seq_images=None, seq_tfs=None):
-    """Slice list of images to skip calculating existing tfs for images we already have."""
-
-    return slice(1 if seq_tfs.shape == (4, 4) else len(seq_tfs), len(seq_images))
-
-
-def shape_check(tf):
-    if tf.shape == (4, 4):
-        tf = tf.reshape(1, 4, 4)
-    return tf
--- scripts/run_navigan.py
+++ scripts/run_navigan.py
@@ -1,21 +1,16 @@
 import time
-
-import numpy as np
 import rospy
 import sys
 
 sys.path.insert(0, '/home/administrator/code/aru_sil_py/navigation/__init__.py')
 sys.path.insert(0, '/home/administrator/code/sgan')
 import argparse
-import os
+
 import torch
-from navigation.nav_options import NavOptions
 # from aru_sil_py.navigation.nav_options import NavOptions
 from navigan_navigator import Navigator
 
-from pathlib import Path
 
-from scripts.goal import pts_to_tfs
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model_path', type=str)
@@ -27,20 +22,10 @@ _DEVICE_ = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 def main(args):
     # ----------------------------------------------
     # Navigator setup
-    nav_args = NavOptions().parse()
-    nav_args.max_x = 0.1
-    nav_args.max_z = 0.35
-    nav = Navigator(nav_args, args.model_path, rate=5)
+    nav = Navigator(args.model_path, rate=5)
 
     # -----------------------------------------------
     count = 0
-    tf = np.eye(4)
-    tf[2, 3] = 0.5
-    tf[0, 3] = 0
-    tf1 = np.eye(4)
-    tf1[2, 3] = 0.5
-    tf1[0, 3] = 0.2
-    tfs = [tf for _ in range(8)]
     print('Waiting for wheel odometry')
     while not nav.odom_callback_status:
         pass
@@ -51,23 +36,14 @@ def main(args):
     #while not nav.goal_status:
     #    time.sleep(0.1)
     #print('Goal received')
-    # print(nav.obs_traj[::, 0].T)
-    # print()
-    # sys.exit(0)
-    goal_tfs = []
-    accum_time = 0
-    # filename = f'{time.time():.0f}_traj.txt'
+
 
     while not rospy.is_shutdown():
         start = time.perf_counter()
         # x, y = (t.item() for t in obs_traj[-1, 0])
         # if len(goal_tfs) < 8:
-        pred, pred_rel = nav.seek_live_goal(title=f'Jan_{count}', filename=filename)
-        # goal_tfs = list(pts_to_tfs(pred_rel))
-        nav.goal_step(pred[11])
-        # nav.sleep()
-        # with np.printoptions(precision=2, suppress=True):
-        #     print(nav.obs_traj.T)
+        pred, pred_rel = nav.seek_live_goal(title=f'Jan_{count}')
+        nav.goal_step(pred)
         print(f"Loop rate {1 / (time.perf_counter() - start):.2f}Hz")
         accum_time += time.perf_counter() - start
 
